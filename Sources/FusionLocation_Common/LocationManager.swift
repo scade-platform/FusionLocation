f1bf96cbc16c827f9c38768073643395d43e6c4d
Pavlo Boiko <boiko.impul@gmail.com>
2021-07-18 16:07:05 +0300

    fix style

--- Sources/FusionLocation_Common/LocationManager.swift
+++ Sources/FusionLocation_Common/LocationManager.swift
@@ -6,38 +6,79 @@ public enum LocationUsage {
 }
 
 public protocol LocationManagerProtocol {
-    /*     *  Creates and returns a new LocationManager object initialized with the LocationUsage.     *       *  Parameters:  usage    	It targets only for iOS. 
-     *							It is used when call requestAuthorization.
-     * 							If always, it calls requestAlwaysAuthorization. If whenInUse, it calls requestWhenInUseAuthorization.     */
+    /*
+     *  Creates and returns a new LocationManager object initialized with the LocationUsage.
+     *  
+     *  Parameters:  usage    	It targets only for iOS
+     *  						It is used when call requestAuthorization.
+     *  						If always, it calls requestAlwaysAuthorization. If whenInUse, it calls requestWhenInUseAuthorization.
+     */
     init(usage: LocationUsage)
 
-    /*     *  requestAuthorization     *     *  Discussion:     *      Request a authorization.     *     *      Calling this method will trigger a prompt to request usage     *      authorization from the user.
-     */
+    /*
+     * @property requestAuthorization
+     *
+     * @discussion Request a authorization.
+     *				Calling this method will trigger a prompt to request usage
+     *          	authorization from the user.
+     */  
     func requestAuthorization()
     
-    /*     *  requestCurrentLocation     *     *  Discussion:     *      Request a single location update.     *     *      The service will attempt to determine location. The location update will be delivered     *      via the receiver     *     *      If no location can be determined, the receiver will be delivered with location nil.     *     *      There can only be one outstanding location request and this method can     *      not be used concurrently with startUpdatingLocation  Calling either of those methods will     *      immediately cancel the location request.  The method     *      stopUpdatingLocation can be used to explicitly cancel the request.     */    
+    /*
+     * @property requestCurrentLocation
+     *
+     * @discussion Request a single location update.
+     *				The service will attempt to determine location. The location update will be delivered
+     *          	via the receiver
+     *				
+     *				If no location can be determined, the receiver will be delivered with location nil.
+     *				
+     *				There can only be one outstanding location request and this method can
+     *				not be used concurrently with startUpdatingLocation  Calling either of those methods will
+     *				immediately cancel the location request.  The method
+     *				topUpdatingLocation can be used to explicitly cancel the request.
+     */
     func requestCurrentLocation(receiver: @escaping (Location?) -> Void)
     
-    /*     *  startUpdatingLocation     *       *  Discussion:     *      Start updating locations.
-     *      The service will attempt to determine location. The location update will be delivered     *      via the receiver     *     *      If no location can be determined, the receiver will be delivered with location nil.     */    
+    /*
+     * @property startUpdatingLocation
+     *
+     * @discussion Start updating locations.
+     *				The service will attempt to determine location. The location update will be delivered
+     *          	via the receiver
+     *				
+     *				If no location can be determined, the receiver will be delivered with location nil.
+     */
     func startUpdatingLocation(receiver: @escaping (Location?) -> Void)
-    
-    /*     *  stopUpdatingLocation     *       *  Discussion:     *      Stop updating locations.     */    
+
+    /*
+     * @property stopUpdatingLocation
+     *
+     * @discussion Stop updating locations.
+     */
     func stopUpdatingLocation()
-    
-    /*     *  distanceBetween     *  
-     *  Parameters:  location1  from location
-     *				 location2  to location
+
+    /*
+     * @property distanceBetween
      *
-     *  Return Value: meter distance
-     *				      *  Discussion:     *      Calculate the distance of two locations.     */        
+     * @discussion Calculate the distance of two locations.
+     *
+     * @param 	location1  from location.
+     *			location2  to location
+     *
+     * @result 	meter distance
+     */
     func distanceBetween(from location1: Location, to location2: Location) -> Double
-    
-     /*     *  bearingBetween     *  
-     *  Parameters:  location1  from location
-     *				 location2  to location
+
+    /*
+     * @property bearingBetween
      *
-     *  Return Value: degree
-     *				      *  Discussion:     *      Calculate the bearing of two locations.     */    
+     * @discussion Calculate the bearing of two locations.
+     *
+     * @param 	location1  from location.
+     *			location2  to location
+     *
+     * @result 	degree
+     */
     func bearingBetween(from location1: Location, to location2: Location) -> Double
 }
